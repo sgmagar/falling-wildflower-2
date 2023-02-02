17ba7e9220ec677677112bdf554abf8aa8c008fd
Saurav <sp.gharti@gmail.com>
2023-02-02 03:18:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-wildflower-2",
+  url: "https://falling-wildflower-2.botics.co",
+  api: "https://falling-wildflower-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
